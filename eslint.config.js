57a07e2706f2816b6b161ac4be3d4779e47a5849
Rodrigo Romero Corvera <rocobros21@gmail.com>
2026-08-17 20:53:22 -0600

    chore: initial monorepo setup

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/.expo/**'],
+  },
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+);
